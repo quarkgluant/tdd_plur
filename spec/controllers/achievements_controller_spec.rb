29b3db2e2d1e1ebbf99f52dc9683c89abe6a3a5c
Patrice Cadiot <pathibul.r@gmail.com>
2018-06-11 16:30:47 +0200

    controller test for create action

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -24,4 +24,32 @@ describe AchievementsController do
       expect(assigns(:achievement)).to eq(achievement)
     end
   end
+
+  describe "POST create" do
+    context "valid data" do
+      let(:valid_data) { FactoryBot.attributes_for(:public_achievement) }
+      it "redirects to achievements#show" do
+        post :create, params: { achievement: valid_data }
+        expect(response).to redirect_to(achievement_path(assigns(:achievement)))
+      end
+      it "creates new achievement in database" do
+        expect do
+          post :create, params: { achievement: valid_data }
+        end.to change(Achievement, :count).by(1)
+      end
+    end
+    
+    context "invalid data" do
+      let(:invalid_data) { FactoryBot.attributes_for(:public_achievement, title: '') }
+      it "renders :new template" do
+        post :create, params: { achievement: invalid_data }
+        expect(response).to render_template(:new)
+      end
+      it "doesn't create new achievement in the database" do
+        expect do
+          post :create, params: { achievement: invalid_data }
+        end.not_to change(Achievement, :count)
+      end
+    end
+  end
 end
