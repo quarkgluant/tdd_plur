d44b4dcad3163d94582c750a5b9c1b931f23516b
Patrice Cadiot <pathibul.r@gmail.com>
2018-06-11 16:30:47 +0200

    controller test for create action

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,7 +1,7 @@
 class Achievement < ApplicationRecord
   validates :title, presence: true
 
-  enum privacy:[ :public_access, :private_access, :friends_access]
+  enum privacy: [ :public_access, :private_access, :friends_access ]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :achievement do
     sequence(:title) { |n| "Achievement #{n}" }
     description "description"
-    privacy Achievement.privacies[:private_access]
+    privacy 'private_access'
     featured false
     cover_image "some_file.png"
 
     factory :public_achievement do
-      privacy Achievement.privacies[:public_access]
+      privacy 'public_access'
     end
   end
 end
